de70c1a6525740441642e5e8c4f0a2ba25f00362
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-20 07:57:24 -0600

    video 1 & 2 working

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+ apiKey: "AIzaSyC8V87tWYlZR40FUY-5uJIQ08ZAuS16ls8",
+     authDomain: "vuefs-dev-8d277.firebaseapp.com",
+     databaseURL: "https://vuefs-dev-8d277.firebaseio.com",
+     projectId: "vuefs-dev-8d277",
+     storageBucket: "vuefs-dev-8d277.appspot.com",
+     messagingSenderId: "128032868730",
+     appId: "1:128032868730:web:69b824b62a7efaa1b66ec3",
+     measurementId: "G-MXZ1ZQGRMK" 
+}

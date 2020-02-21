8d942faaaef8dcb63dbbd6ad8ad2f69e4977dc90
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-21 07:54:30 -0600

    working dashboard

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
-import firebaseConfig from './'
+import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 export default firebaseApp.firestore()
